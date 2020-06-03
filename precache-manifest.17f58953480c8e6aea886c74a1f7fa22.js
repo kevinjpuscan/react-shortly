4ecf166ab872818fe4d4b1a37dca483d8c336f7c
kevin puscan <kevinjpuscan@gmail.com>
2020-06-03 16:26:37 -0500

    Updates

---
+++ precache-manifest.17f58953480c8e6aea886c74a1f7fa22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2228f1efd883ce05a68f6810edd0f83",
+    "url": "/react-shortly/index.html"
+  },
+  {
+    "revision": "40cfb3569800d840be45",
+    "url": "/react-shortly/static/css/main.367e869b.chunk.css"
+  },
+  {
+    "revision": "822a6ce695869a20f46c",
+    "url": "/react-shortly/static/js/2.ec2f5997.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-shortly/static/js/2.ec2f5997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cfb3569800d840be45",
+    "url": "/react-shortly/static/js/main.bde98135.chunk.js"
+  },
+  {
+    "revision": "952676b079efbb8204f5",
+    "url": "/react-shortly/static/js/runtime-main.4ec272da.js"
+  }
+]);
